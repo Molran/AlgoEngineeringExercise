7b086dec8062bfc0d13c8af5ef4840fcaa191267
Thoralf Ehret <Thoralf@live.de>
2014-11-06 12:16:13 +0100

    Inluding googletest, excluding Visual Studio
    got googletest to work, so i dont need assert and/or manual input
    anymore to test. Also removed german comments made by Visual Studio, and
    removed Visual Studio created files.

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,10 +1,8 @@
-// Fibonacci.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include <assert.h>
 #include <vector>
+#include <gtest\gtest.h>
 using namespace std;
 
 int fib(int x){		//code derived from definition
@@ -47,25 +45,48 @@ int fib3(int x){	//linear time, constant memory
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int input;
+	testing::InitGoogleTest(&argc, argv);
 
-	assert(fib(3) == 2);
-	assert(fib(4) == 3);
-	assert(fib(5) == 5);
-	assert(fib(6) == 8);
-	assert(fib(7) == 13);
-	assert(fib(8) == 21);
-	assert(fib(9) == 34);
-
-
-	for (int i = 1; i > 0;){
+	/*for (int i = 1; i > 0;){
 		cout << "Bitte geben sie die Nummer der zu berechnenden Fibonacci-Zahl an: ";
 		cin >> input;
 		cout << "Das Ergebnis von fib ist " << fib(input) << ".\n";
 		cout << "Das Ergebnis von fib2 ist " << fib2(input) << ".\n";
 		cout << "Das Ergebnis von fib3 ist " << fib3(input) << ".\n";
 
-	}
+	}*/
 
+	RUN_ALL_TESTS();
 	cin >> input;
 	return 0;
+}
+
+TEST(fibTest, TestCorrectComputation){
+	EXPECT_EQ(0, fib(0));
+	EXPECT_EQ(1, fib(1));
+	EXPECT_EQ(1, fib(2));
+	EXPECT_EQ(2, fib(3));
+	EXPECT_EQ(55, fib(10));
+	EXPECT_EQ(6765, fib(20));
+	EXPECT_EQ(832040, fib(30));
+}
+
+TEST(fib2Test, TestCorrectComputation){
+	EXPECT_EQ(0, fib2(0));
+	EXPECT_EQ(1, fib2(1));
+	EXPECT_EQ(1, fib2(2));
+	EXPECT_EQ(2, fib2(3));
+	EXPECT_EQ(55, fib2(10));
+	EXPECT_EQ(6765, fib2(20));
+	EXPECT_EQ(832040, fib2(30));
+}
+
+TEST(fib3Test, TestCorrectComputation){
+	EXPECT_EQ(0, fib3(0));
+	EXPECT_EQ(1, fib3(1));
+	EXPECT_EQ(1, fib3(2));
+	EXPECT_EQ(2, fib3(3));
+	EXPECT_EQ(55, fib3(10));
+	EXPECT_EQ(6765, fib3(20));
+	EXPECT_EQ(832040, fib3(30));
 }
