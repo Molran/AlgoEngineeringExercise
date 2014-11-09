854a251c9e3d31563d19485da419ba8e38d726ec
Thoralf Ehret <Thoralf@live.de>
2014-11-09 20:30:53 +0100

    Added lookup table, fib5() and fib6()
    fib4() still needs work. added functions to write and read the
    lookupfile fibNumbers.txt. also changed return values of fib methods to
    long long int, so that we can compute at least some extra fibonacci
    numbers

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,8 +1,13 @@
-#include "stdafx.h"
+#include "stdafx.h"	//visual studio created file. it's meant to decrease compiling time, but this program is short, so it's empty
 #include <iostream>
-#include <vector>
+#include <vector>	//for fib4
 #include <gtest\gtest.h>
+#include <math.h>	//for fib5
+#include <iostream>	//for fibToText
+#include <fstream>
 using namespace std;
+
+long long int fibLUT [90];
 
 class Matrix{
 	vector<vector<int>> mat;
@@ -61,14 +66,14 @@ Matrix Matrix::mult(Matrix x){	//multiplication of given matrix * x, simple impl
 	return resM;
 }
 
-int fib(int x){		//code derived from definition
+long long int fib(int x){		//code derived from definition
 	if (x == 0) return 0;
 	if (x == 1) return 1;
 	return fib(x - 1) + fib(x - 2);
 }
 
-int fib2(int x){	//linear running time and memory
-	vector<int> fibs;
+long long int fib2(int x){	//linear running time and memory
+	vector<long long int> fibs;
 
 	fibs.push_back(0);
 	fibs.push_back(1);
@@ -82,10 +87,10 @@ int fib2(int x){	//linear running time and memory
 	return fibs.back();
 }
 
-int fib3(int x){	//linear time, constant memory
+long long int fib3(int x){	//linear time, constant memory
 	if (x <= 1) return x;
 
-	int fibs[3];
+	long long int fibs[3];
 	fibs[0] = 0;
 	fibs[1] = 1;
 
@@ -98,9 +103,10 @@ int fib3(int x){	//linear time, constant memory
 	return fibs[2];
 }
 
-int fib4(int x){
-	if (x == 0) return 1;
-	//TODO if (x == 1) 
+long long int fib4(int x){	//logarithmic running time
+	if (x == 0) return 0;
+	if (x == 1) return 1;
+
 	vector<vector<int>> helpV1;	//the vector we will use for the 2x2 matrix
 	vector<vector<int>> helpV2;	//the vector we will use for the 2x1 matrix
 	vector<int> tempV;
@@ -119,21 +125,69 @@ int fib4(int x){
 	Matrix helpMatrix1(helpV1);
 	Matrix resMatrix(helpV1);
 	Matrix helpMatrix2(helpV2);
-	if (x % 2 == 0){
-		for (x; x > 1; x / 2){	//we shouldn't need odd or even evaluation if we use the "<"
+	int oddcount = 0;
+
+
+	/*for (x; x >= 1;){
+		if (x == 1){
+			resMatrix = helpMatrix1.mult(resMatrix);
+			x--;
+		}else if ((x % 2) == 0){
 			resMatrix = resMatrix.mult(resMatrix);
+			x = x / 2;
+		}else if ((x % 2) == 1){
+			resMatrix = resMatrix.mult(resMatrix.mult(resMatrix));
+			x = (x - 1) / 2;
+			oddcount++;
 		}
-	}
+	}*/
+
+	/*for (oddcount; oddcount > 0; oddcount--){
+		resMatrix = helpMatrix1.mult(resMatrix);
+	}*/
+
 	resMatrix = resMatrix.mult(helpMatrix2);
 	return resMatrix.getElement(0, 0);
+}
+
+int fib5(int x){	//formula derived from lecture, no matrix needed
+	double res = floor((1.0 / pow(5.0, 0.5)) * pow(((1.0 + pow(5.0, 0.5)) / 2.0), x) + 0.5);
+	return res;
+}
+
+int fib6(int x){	//taking information from lookup table, if not, use fib5() to calculate
+	if(x< sizeof(fibLUT))return fibLUT[x];
+	return fib5(x);
+}
+
+void fibToText(){	//creates a txt file with 90 fibonacci numbers, you can read it in linear time with getline()
+	ofstream fibText;
+	fibText.open("fibNumbers.txt");
+	for (int i = 0; i <= 90; i++) fibText << fib3(i) << "\n";
+	fibText.close();
+}
+
+void initFibLUT(){	//open textfile, read the strings, convert them to int, then save them to fibLUT
+	ifstream fibText;
+	fibText.open("fibNumbers.txt");	
+	string fibString;
+	int fibNum;
+	for (int i = 0; i < 90; i++){
+		getline(fibText, fibString);
+		stringstream conv(fibString);
+		conv >> fibNum;
+		fibLUT[i] = fibNum;
+	}
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int input;
+	initFibLUT();
 	testing::InitGoogleTest(&argc, argv);
 
 	//TESTING GROUND
+	/*
 	vector<vector<int>> testV;
 	vector<vector<int>> testV2;
 	vector<int> tempV;
@@ -152,24 +206,24 @@ int _tmain(int argc, _TCHAR* argv[])
 	tempV.push_back(1);
 	testV2.push_back(tempV);
 
-	//cout << testV[0][0] << testV[0][1] << testV[1][0] << testV[1][1];		//reassuring that building a vector works as i wish
-
 	Matrix testM (testV);
 	Matrix testM2(testV2);
 	//cout << testM2.toString();
-	cout << testM.mult(testM2).toString();
+	//cout << testM.mult(testM2).toString();
 	//cout << testM.toString();		//reassuring the Matrix works, too
 
-	//TESTING GROUND
+	*/
+	//TESTING GROUND END
 
-	/*for (int i = 1; i > 0;){
+	for (int i = 1; i > 0;){
 		cout << "Bitte geben sie die Nummer der zu berechnenden Fibonacci-Zahl an: ";
 		cin >> input;
 		cout << "Das Ergebnis von fib ist " << fib(input) << ".\n";
 		cout << "Das Ergebnis von fib2 ist " << fib2(input) << ".\n";
 		cout << "Das Ergebnis von fib3 ist " << fib3(input) << ".\n";
 		cout << "Das Ergebnis von fib4 ist " << fib4(input) << ".\n";
-	}*/
+		cout << "Das Ergebnis von fib4 ist " << fib5(input) << ".\n";
+	}
 
 	//RUN_ALL_TESTS();
 	cin >> input;
@@ -204,4 +258,14 @@ TEST(fib3Test, TestCorrectComputation){
 	EXPECT_EQ(55, fib3(10));
 	EXPECT_EQ(6765, fib3(20));
 	EXPECT_EQ(832040, fib3(30));
+}
+
+TEST(fib5Test, TestCorrectComputation){
+	EXPECT_EQ(0, fib5(0));
+	EXPECT_EQ(1, fib5(1));
+	EXPECT_EQ(1, fib5(2));
+	EXPECT_EQ(2, fib5(3));
+	EXPECT_EQ(55, fib5(10));
+	EXPECT_EQ(6765, fib5(20));
+	EXPECT_EQ(832040, fib5(30));
 }
